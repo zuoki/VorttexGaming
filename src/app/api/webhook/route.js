32d474b0e60d6c8b3b378f968f6f9866ca5d4953
Diaz027 <dami27cito@gmail.com>
2023-12-06 16:09:23 -0300

    delete users

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -2,23 +2,79 @@ import { NextResponse } from "next/server";
 import  {prisma } from '@/libs/prisma';
 
 export async function POST(request) {
-  const {data} = await request.json();
-  console.log("POSTEANDO INFO..");
-  console.log(data);
-  if (data ) {
-    // Insertar nuevo usuario en la base de datos
-    const newUser = await prisma.user.create({
-      data: {
-        user_id: data?.id,
-        email:  data?.email_addresses[0]?.email_address,
-        username: data?.username
-      },
-    });
-    console.log("Usuario creado!!!!!!!", newUser);
-    
-    return NextResponse.json(newUser);
-  } else {
-    console.log("No se pudo crear el usuario. data o data.user no existen.");
-  }
+  const { type, data} = await request.json();
+  console.log("newwwwwwwwwwwww", type, data);
+
+  const id = data?.id;
+
+  switch (type) {
+    case 'user.deleted':
+      console.log("ELIMINANDO USUARIO..");
+      if (id) {
+        const deletedUser = await prisma.user.update({
+          where: { user_id: id },
+          
+            active: false
+          
+        });
+        console.log("Usuario eliminado!!!!!!!", deletedUser);
+        return NextResponse.json(deletedUser);
+      } else {
+        console.log("No se pudo eliminar el usuario. id no existe.");
+      }
+      break;
+
+    case 'user.updated':
+      console.log("ACTUALIZANDO USUARIO..");
+      if (id && data) {
+        const updatedUser = await prisma.user.update({
+          where: { user_id: id },
+          data: {
+            email: data?.email,
+            username: data?.username,
+          },
+        });
+        console.log("Usuario actualizado!!!!!!!", updatedUser);
+        return NextResponse.json(updatedUser);
+      } else {
+        console.log("No se pudo actualizar el usuario. id o data no existen.");
+      }
+      break;
+
+    case 'user.created':
+      console.log("CREANDO USUARIO..");
+      if (data) {
+        const existingUser = await prisma.user.findUnique({
+          where: { user_id: id },
+        });
   
+        if (existingUser && existingUser.active === false) {
+          const updatedUser = await prisma.user.update({
+            where: { user_id: id },
+            data: {
+              active: true
+            },
+          });
+          console.log("Usuario existente actualizado!!!!!!!", updatedUser);
+          return NextResponse.json(updatedUser);
+        } else {
+          const newUser = await prisma.user.create({
+            data: {
+              user_id: data?.id,
+              email:  data?.email_addresses[0]?.email_address,
+              username: data?.username
+            },
+          });
+          console.log("Usuario creado!!!!!!!", newUser);
+          return NextResponse.json(newUser);
+        }
+      } else {
+        console.log("No se pudo crear el usuario. data o data.user no existen.")
+      }
+      break;
+
+    default:
+      console.log("type no reconocida.");
+      break;
+  }
 }
