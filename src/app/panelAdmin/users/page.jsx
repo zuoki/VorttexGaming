06bd93215a946170f5b97a20c0c8bf4b809e8bb5
santiclfriedrich <santiclear1@hotmail.com>
2023-12-06 16:56:55 -0300

    users panel

--- src/app/panelAdmin/users/page.jsx
+++ src/app/panelAdmin/users/page.jsx
@@ -1,9 +1,57 @@
-const page = () => {
-  return (
-    <div>
-      <h1>USERS</h1>
-    </div>
-  )
-}
+"use client"
+import React, { useState } from 'react';
+import './users.css';
 
-export default page
+const Page = () => {
+  
+  const [users, setUsers] = useState([
+    { id: 1, name: 'Dami puto', email: 'usuario1@example.com' },
+    { id: 2, name: 'Santeta', email: 'usuario2@example.com' },
+    { id: 3, name: 'Translate', email: 'usuario3@example.com' },
+  ]);
+
+  // elimina un usuario por id
+  const deleteUser = (userId) => {
+    const updatedUsers = users.filter((user) => user.id !== userId);
+    setUsers(updatedUsers);
+  };
+
+  // renderizar las filas de la tabla
+  const renderRows = () => {
+    return users.map((user) => (
+      <tr key={user.id}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>
+          
+          <button onClick={() => deleteUser(user.id)}>🗑️</button>
+        </td>
+      </tr>
+    ));
+  };
+
+  return (
+    <div className='userCont'>
+      <h1>USERS</h1>
+
+      <div className='userDiv'>
+        
+        <table>
+
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Email</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+
+          <tbody>{renderRows()}</tbody>
+        </table>
+
+      </div>
+    </div>
+  );
+};
+
+export default Page;
