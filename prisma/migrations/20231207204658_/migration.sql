582b99bc4730c963c5e073271548f6467754ffac
Diaz027 <dami27cito@gmail.com>
2023-12-07 18:13:32 -0300

    users

--- prisma/migrations/20231207204658_/migration.sql
+++ prisma/migrations/20231207204658_/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'USER');
+
 -- CreateTable
 CREATE TABLE "Games" (
     "id" SERIAL NOT NULL,
@@ -25,6 +28,7 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,
     "username" TEXT,
     "active" BOOLEAN NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
     "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -31,6 +31,7 @@ export async function POST(request) {
           data: {
             email: data?.email,
             username: data?.username,
+            role: data?.publicMetadata?.role,
           },
         });
         return NextResponse.json(updatedUser);
@@ -53,6 +54,8 @@ export async function POST(request) {
               data: {
                 active: true,
                 user_id: data?.id,
+                username: data?.username,
+                role: data?.publicMetadata?.role,
               },
             });
             console.log("actualizandooooooo", updatedUser);
@@ -67,6 +70,7 @@ export async function POST(request) {
               email: data?.email_addresses[0]?.email_address,
               username: data?.username,
               active: true,
+              role: data?.publicMetadata?.role,
             },
           });
           return NextResponse.json(newUser);
