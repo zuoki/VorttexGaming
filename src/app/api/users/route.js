174ad29c7a8a32187a3e6ba67da8c727b04cb0dc
Diaz027 <dami27cito@gmail.com>
2023-12-11 11:51:35 -0300

    get de users and true

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,8 +1,11 @@
-import { NextResponse } from 'next/server';
-import  {prisma } from '@/libs/prisma';
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
 export async function GET() {
-    const allProfile = await prisma.user.findMany();
-    return NextResponse.json(allProfile);
-  }
-
+  const allProfile = await prisma.user.findMany({
+    where: {
+      active: true,
+    },
+  });
+  return NextResponse.json(allProfile);
+}
