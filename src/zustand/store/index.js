35496bcdcd3d7253e540a9eac61d7d031a265965
Diaz027 <dami27cito@gmail.com>
2023-12-12 17:41:01 -0300

    disploy

--- src/zustand/store/index.js
+++ src/zustand/store/index.js
@@ -5,50 +5,49 @@ export const useStoreCart = create((set) => ({
   data: [],
   userId: null,
   gamesInCart: [],
-  
+
   setUserId: (id) =>
-  set((state) => {
-    const storedGamesInCart =
-    typeof window !== "undefined"
-    ? JSON.parse(localStorage.getItem("gamesInCart" + id))
-    : [];
-    return { userId: id, gamesInCart: storedGamesInCart || [] };
-  }),
-  
+    set((state) => {
+      const storedGamesInCart =
+        typeof window !== "undefined"
+          ? JSON.parse(localStorage.getItem("gamesInCart" + id))
+          : [];
+      return { userId: id, gamesInCart: storedGamesInCart || [] };
+    }),
+
   //Storage
   addGamesToCart: (games) => {
     set((state) => {
       if (state.gamesInCart.length < 4) {
         const newGamesInCart = [...state.gamesInCart, games];
         Swal.fire({
-          icon: 'success',
-          iconColor: 'green', // Cambia el color del icono a amarillo
+          icon: "success",
+          iconColor: "green", // Cambia el color del icono a amarillo
           titleText: "GAME ADD TO CART",
-          background: '#333333', // Color de fondo negro
+          background: "#333333", // Color de fondo negro
           html: '<span style="color: orange;"></span>', // Cambia el color del texto a blanco
-          footer: '<a href="http://localhost:3000/cart">Success! The game has been added to your shopping cart.</a>',
-          
+          footer:
+            '<a href="http://localhost:3000/cart">Success! The game has been added to your shopping cart.</a>',
         });
         if (typeof window !== "undefined") {
           localStorage.setItem(
             "gamesInCart" + state.userId,
             JSON.stringify(newGamesInCart)
-            );
-          }
-          return { gamesInCart: newGamesInCart };
+          );
+        }
+        return { gamesInCart: newGamesInCart };
       } else {
         Swal.fire({
-          icon: 'warning',
-          iconColor: 'yellow', // Cambia el color del icono a amarillo
+          icon: "warning",
+          iconColor: "yellow", // Cambia el color del icono a amarillo
           titleText: "WARNING...",
-          background: '#333333', // Color de fondo negro
+          background: "#333333", // Color de fondo negro
           html: '<span style="color: orange;">No more than 4 games can be added in a single purchase</span>', // Cambia el color del texto a blanco
-          footer: '<a href="http://localhost:3000/cart">Complete your cart purchase or remove the games.</a>',
-          confirmButtonColor: '#3333;', // Cambia el color del botón de confirmación a negro
+          footer:
+            '<a href="http://localhost:3000/cart">Complete your cart purchase or remove the games.</a>',
+          confirmButtonColor: "#3333;", // Cambia el color del botón de confirmación a negro
         });
-        
-        
-        
+
         return state;
       }
     });
@@ -68,7 +67,7 @@ export const useStoreCart = create((set) => ({
       return { gamesInCart: newGamesInCart };
     });
   },
-  
+
   emptyCart: () => {
     set((state) => {
       if (typeof window !== "undefined") {
@@ -82,7 +81,6 @@ export const useStoreCart = create((set) => ({
     if (typeof window === "undefined") return;
     set((state) => {
       return { ...state, data: games };
-    })
+    });
   },
 }));
-
