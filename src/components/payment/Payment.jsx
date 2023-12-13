a953106c4dc3fac0320881fd45b8e82c8fe093fe
Diaz027 <dami27cito@gmail.com>
2023-12-13 12:18:07 -0300

    userLicense

--- src/components/payment/Payment.jsx
+++ src/components/payment/Payment.jsx
@@ -18,17 +18,16 @@ const Payment = () => {
   let id;
 
   if (gamesInCart.length > 0) id = gamesInCart[0].id;
-  console.log("soy id ", id);
 
-  // const API_SEND_LICENSE_URL =
-  //   process.env.NODE_ENV === "development"
-  //     ? process.env.NEXT_PUBLIC_URL_REQUESTS_LICENSE_LOCAL
-  //     : process.env.NEXT_PUBLIC_URL_REQUESTS_LICENSE_DEPLOY;
+  const API_SEND_USER_LICENSE_URL =
+    process.env.NODE_ENV === "development"
+      ? process.env.NEXT_PUBLIC_URL_REQUESTS_USER_LICENSE_LOCAL
+      : process.env.NEXT_PUBLIC_URL_REQUESTS_USER_LICENSE_DEPLOY;
 
-  // const API_URL =
-  //   process.env.NODE_ENV === "development"
-  //     ? process.env.NEXT_PUBLIC_URL_REQUESTS_PAYMENT_LOCAL
-  //     : process.env.NEXT_PUBLIC_URL_REQUESTS_PAYMENT_DEPLOY;
+  const API_URL =
+    process.env.NODE_ENV === "development"
+      ? process.env.NEXT_PUBLIC_URL_REQUESTS_PAYMENT_LOCAL
+      : process.env.NEXT_PUBLIC_URL_REQUESTS_PAYMENT_DEPLOY;
 
   return (
     <div className="paypal">
@@ -44,7 +43,7 @@ const Payment = () => {
               label: "pay",
             }}
             createOrder={async () => {
-              const res = await fetch("/api/checkout", {
+              const res = await fetch(API_URL, {
                 method: "POST",
               });
               const order = await res.json();
@@ -66,7 +65,7 @@ const Payment = () => {
               // Vacía el carrito de compras después de que se haya realizado un pago exitoso
 
               const response = await axios.put(
-                "api/userLicense",
+                API_SEND_USER_LICENSE_URL,
                 { email, id },
                 {
                   headers: {
@@ -77,15 +76,14 @@ const Payment = () => {
 
               const dataPut = response.data;
               const nameLicense = dataPut.name;
-              console.log("este es el response de put", dataPut);
 
-              // const API_SEND_EMAIL_URL =
-              //   process.env.NODE_ENV === "development"
-              //     ? process.env.NEXT_PUBLIC_URL_REQUESTS_SEND_EMAIL_LOCAL
-              //     : process.env.NEXT_PUBLIC_URL_REQUESTS_SEND_EMAIL_DEPLOY;
+              const API_SEND_EMAIL_URL =
+                process.env.NODE_ENV === "development"
+                  ? process.env.NEXT_PUBLIC_URL_REQUESTS_SEND_EMAIL_LOCAL
+                  : process.env.NEXT_PUBLIC_URL_REQUESTS_SEND_EMAIL_DEPLOY;
 
               await axios.post(
-                "/api/sendEmail",
+                API_SEND_EMAIL_URL,
                 { email, nameLicense },
                 {
                   headers: {
@@ -110,12 +108,7 @@ const Payment = () => {
                 imageAlt: "Custom image",
               });
 
-              // Hacer una solicitud al back-end para enviar un correo electrónico
-              // const API_SEND_EMAIL_URL =
-              //   process.env.NODE_ENV === "development"
-              //     ? process.env.NEXT_PUBLIC_URL_REQUESTS_SEND_EMAIL_LOCAL
-              //     : process.env.NEXT_PUBLIC_URL_REQUESTS_SEND_EMAIL_DEPLOY;
-              const res = await fetch("/api/sendEmail", {
+              const res = await fetch(API_SEND_EMAIL_URL, {
                 method: "POST",
                 headers: {
                   "Content-Type": "application/json",
@@ -133,121 +126,3 @@ const Payment = () => {
 };
 
 export default Payment;
-
-// "use client";
-// import React, { useState } from "react";
-// import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-// import { useUser } from "@clerk/nextjs";
-// import axios from "axios";
-// import Swal from "sweetalert2";
-// import { useStoreCart } from "@/zustand/store";
-
-// const Payment = () => {
-//   const [statebuy, setStateBuy] = useState(" COMPRA EN PROGRESO ");
-//   const [css, setCss] = useState("progress");
-//   const data = useUser();
-//   const email = data?.user?.emailAddresses?.[0]?.emailAddress;
-//   // const email = "riosdeborasabrina@gmail.com";
-//   const { emptyCart, gamesInCart } = useStoreCart();
-//   let id;
-//   if (gamesInCart.length > 0) id = gamesInCart[0].id;
-
-//   return (
-//     <div className="paypal">
-//       <div className="visualP">
-//         <h1 className={css}>{statebuy}</h1>
-//         <PayPalScriptProvider
-//           options={{
-//             clientId:
-//               "AVwb2hp2ZMkuRiQgJ1GujcCeizroHgzH-pOwUlWYdCNmPNcgRPaDjwrVg4BfI_k98Qd4DtUVpsYCquD8",
-//           }}
-//         >
-//           <PayPalButtons
-//             style={{
-//               label: "pay",
-//             }}
-//             createOrder={async () => {
-//               const res = await fetch("/api/checkout", {
-//                 method: "POST",
-//               });
-//               const order = await res.json();
-//               return order.id;
-//             }}
-//             onApprove={async (data, actions) => {
-//               Swal.fire({
-//                 background: "#fff",
-//                 title: "Success!...",
-//                 text: "You will be sent the codes for the games purchased",
-//                 imageUrl:
-//                   "https://media0.giphy.com/media/XreQmk7ETCak0/giphy.gif?cid=ecf05e47390bwrvnc8r79ic7r23v8yovq0s5lw4uz7kdcb4u&ep=v1_gifs_search&rid=giphy.gif&ct=g",
-//                 imageWidth: 300,
-//                 imageHeight: 200,
-//                 imageAlt: "Custom image",
-//               });
-//               // agregamos async
-//               await actions.order.capture(); // agregamos await *Debbb
-//               // Vacía el carrito de compras después de que se haya realizado un pago exitoso
-
-//               const response = await axios.put(
-//                 "api/usersLicense",
-//                 { email, id },
-//                 {
-//                   headers: {
-//                     "Content-Type": "application/json",
-//                   },
-//                 }
-//               );
-
-//               const dataPut = response.data;
-//               const nameLicense = dataPut.name;
-
-//               // const API_SEND_EMAIL_URL =
-//               //   process.env.NODE_ENV === "development"
-//               //     ? process.env.NEXT_PUBLIC_URL_REQUESTS_SEND_EMAIL_LOCAL
-//               //     : process.env.NEXT_PUBLIC_URL_REQUESTS_SEND_EMAIL_DEPLOY;
-
-//               await axios.post(
-//                 "/api/sendEmail",
-//                 { email, nameLicense },
-//                 {
-//                   headers: {
-//                     "Content-Type": "application/json",
-//                   },
-//                 }
-//               );
-
-//               emptyCart();
-//               window.location.href = "/";
-//             }}
-//             // fin bloque backend
-//             onCancel={async (data) => {
-//               Swal.fire({
-//                 background: "#fff",
-//                 title: "Opss!...",
-//                 text: "It seems that you have canceled the purchase",
-//                 imageUrl:
-//                   "https://static.vecteezy.com/system/resources/previews/017/396/313/non_2x/illustration-businessman-holding-an-empty-wallet-free-png.png",
-//                 imageWidth: 400,
-//                 imageHeight: 200,
-//                 imageAlt: "Custom image",
-//               });
-
-//               // Hacer una solicitud al back-end para enviar un correo electrónico
-//               await fetch("/api/sendEmail", {
-//                 method: "POST",
-//                 headers: {
-//                   "Content-Type": "application/json",
-//                 },
-//                 body: JSON.stringify({
-//                   email,
-//                 }),
-//               });
-//             }}
-//           />
-//         </PayPalScriptProvider>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Payment;
